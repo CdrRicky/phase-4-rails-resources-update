58c5a7ff3c4e164f8c71e7235dcb752bffbcbdd6
Dev_Ricky <eric.kiogora12@gmail.com>
2022-10-06 08:01:37 +0300

    commit tree a9533ccc05c5288eace61b00b6c949d9be1e5b60

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  # resources :birds, only: [:index, :show, :create, :update]
+  get "/plants", to: "plants#index"
+
+  get "/plants/:id", to: "plants#show"
+
+  post "/plants", to: "plants#create"
 end
